4bbe0a3efc9a25adbfb4acf27a3714df830d12ac
magtesyan <magtesyan@inbox.ru>
2020-06-14 19:38:11 +0300

    prop-types, offerNames from props

--- src/index.js
+++ src/index.js
@@ -3,12 +3,20 @@ import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
 
 const Offers = {
-  OFFERS_COUNT: 312
+  OFFERS_COUNT: 312,
+  OFFERS_NAMES: [
+    `Beautiful & luxurious apartment at great location`,
+    `Wood and stone place`,
+    `Canal View Prinsengracht`,
+    `Nice, cozy, warm big bed apartment`,
+    `Wood and stone place`
+  ]
 };
 
 ReactDOM.render(
     <App
       offersCount={Offers.OFFERS_COUNT}
+      offersNames={Offers.OFFERS_NAMES}
     />,
     document.querySelector(`#root`)
 );
