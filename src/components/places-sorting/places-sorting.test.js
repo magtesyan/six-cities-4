f4e1affbc3d623c23a03126a6d9002d833fb55d9
magtesyan <magtesyan@inbox.ru>
2020-07-14 01:25:40 +0300

    tests for places-sorting added

--- src/components/places-sorting/places-sorting.test.js
+++ src/components/places-sorting/places-sorting.test.js
@@ -4,12 +4,32 @@ import React from "react";
 
 import PlacesSorting from "./places-sorting.jsx";
 
+const sortingOptions = [
+  {
+    name: `Popular`,
+  },
+  {
+    name: `Price: low to high`,
+  },
+  {
+    name: `Price: high to low`,
+  },
+  {
+    name: `Top rated first`,
+  },
+];
+
 it(`Sorting dropdown is rendered correctly`, () => {
+  const onFilterClick = jest.fn();
   const tree = renderer
     .create(
         <BrowserRouter>
           <PlacesSorting
             onSortTypeClick={jest.fn()}
+            onFilterClick={onFilterClick}
+            sortingOptions={sortingOptions}
+            isFilterOpened={true}
+            activeSortingOption={sortingOptions[0].name}
           />
         </BrowserRouter>
     ).toJSON();
